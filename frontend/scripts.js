9ced94b479195eea62945832dd0734b786b0e5ec
Jukka Ylitalo <juylitalo@gmail.com>
2016-11-24 19:21:01 +0200

    format document

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,172 +1,159 @@
 
 
-    
-jQuery( document ).ready( function () {
+
+jQuery(document).ready(function () {
 
 
-			var container, stats;
+    var container, stats;
 
-			var camera, scene, renderer;
+    var camera, scene, renderer;
 
-			var mouseX = 0, mouseY = 0;
+    var mouseX = 0, mouseY = 0;
 
-			var windowHalfX = window.innerWidth / 2;
-			var windowHalfY = window.innerHeight / 2;
+    var windowHalfX = window.innerWidth / 2;
+    var windowHalfY = window.innerHeight / 2;
 
-            var oar_pivot = new THREE.Object3D();
-			init();
-			animate();
-
-            
-
-			function init() {
-
-				container = document.createElement( 'div' );
-				document.body.appendChild( container );
-
-				camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-				camera.position.x = 0;
-                camera.position.y = 120;
-                camera.position.z = 120;
-
-				// scene
-
-				scene = new THREE.Scene();
-
-				var ambient = new THREE.AmbientLight( 0x888888 );
-				scene.add( ambient );
-
-				var directionalLight = new THREE.DirectionalLight( 0xffeedd );
-				directionalLight.position.set( 0, 5, 1 ).normalize();
-				scene.add( directionalLight );
-
-				// model
-                var pivot = new THREE.Object3D();
-                scene.add(pivot);
-                
-                //pivot.rotation.y = 0.5 * Math.PI;
-
-               
-                //oar_pivot.rotation.y = 0.5 * Math.PI;
-                pivot.add(oar_pivot);
-				var onProgress = function ( xhr ) {
-					if ( xhr.lengthComputable ) {
-						var percentComplete = xhr.loaded / xhr.total * 100;
-						console.log( Math.round(percentComplete, 2) + '% downloaded' );
-					}
-				};
-
-				var onError = function ( xhr ) { };
-
-				THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
-
-				var mtlLoader = new THREE.MTLLoader();
-				mtlLoader.setPath( '3d/' );
-				mtlLoader.load( 'boat3.mtl', function( materials ) {
-
-					materials.preload();
-                    
-					var objLoader = new THREE.OBJLoader();
-					objLoader.setMaterials( materials );
-					objLoader.setPath( '3d/' );
-					objLoader.load( 'boat3.obj', function ( object ) {
-                   // object.doubleSided = true;
-                   console.dir(object);
-                   object.children[0].material.side = THREE.DoubleSide;
-                   object.children[1].material.side = THREE.DoubleSide;
-                   object.children[2].material.side = THREE.DoubleSide;
-
-                        
-						pivot.add( object );
-
-					}, onProgress, onError );
-
-				});
-
-		        mtlLoader = new THREE.MTLLoader();
-				mtlLoader.setPath( '3d/' );
-				mtlLoader.load( 'oar4.mtl', function( materials ) {
-
-					materials.preload();
-                    
-					var objLoader = new THREE.OBJLoader();
-					objLoader.setMaterials( materials );
-					objLoader.setPath( '3d/' );
-					objLoader.load( 'oar4.obj', function ( object2 ) {
-                   // object.doubleSided = true;
-      
- 
-						 
-                        //object.rotation.y = 0.5 * Math.PI;
-                        // console.dir(scene);
-                         //object2.rotation.z = 0.5 * Math.PI;
-                         object2.position.z = -13.5;
-                         object2.position.x = -22.7;
-						oar_pivot.add( object2 );
-                        
-                        
-					}, onProgress, onError );
-
-				});
-
-                oar_pivot.position.z = 13.5;
-                oar_pivot.position.x = 22.7;
+    var oar_pivot = new THREE.Object3D();
+    init();
+    animate();
 
 
-				//
 
-				renderer = new THREE.WebGLRenderer();
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-                
-                renderer.setClearColor( 0xFFFFFF );
-				container.appendChild( renderer.domElement );
+    function init() {
 
-				document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+        container = document.createElement('div');
+        document.body.appendChild(container);
 
-				//
+        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+        camera.position.x = 0;
+        camera.position.y = 120;
+        camera.position.z = 120;
 
-				window.addEventListener( 'resize', onWindowResize, false );
+        // scene
 
-			}
+        scene = new THREE.Scene();
 
-			function onWindowResize() {
+        var ambient = new THREE.AmbientLight(0x888888);
+        scene.add(ambient);
 
-				windowHalfX = window.innerWidth / 2;
-				windowHalfY = window.innerHeight / 2;
+        var directionalLight = new THREE.DirectionalLight(0xffeedd);
+        directionalLight.position.set(0, 5, 1).normalize();
+        scene.add(directionalLight);
 
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
+        // model
+        var pivot = new THREE.Object3D();
+        scene.add(pivot);
 
-				renderer.setSize( window.innerWidth, window.innerHeight );
+        pivot.add(oar_pivot);
+        var onProgress = function (xhr) {
+            if (xhr.lengthComputable) {
+                var percentComplete = xhr.loaded / xhr.total * 100;
+                console.log(Math.round(percentComplete, 2) + '% downloaded');
+            }
+        };
 
-			}
+        var onError = function (xhr) { };
 
-			function onDocumentMouseMove( event ) {
+        THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
 
-				mouseX = ( event.clientX - windowHalfX ) / 8;
-				mouseY = ( event.clientY - windowHalfY ) / 8;
+        var mtlLoader = new THREE.MTLLoader();
+        mtlLoader.setPath('3d/');
+        mtlLoader.load('boat3.mtl', function (materials) {
 
-			}
+            materials.preload();
 
-			//
+            var objLoader = new THREE.OBJLoader();
+            objLoader.setMaterials(materials);
+            objLoader.setPath('3d/');
+            objLoader.load('boat3.obj', function (object) {
+                // object.doubleSided = true;
+                console.dir(object);
+                object.children[0].material.side = THREE.DoubleSide;
+                object.children[1].material.side = THREE.DoubleSide;
+                object.children[2].material.side = THREE.DoubleSide;
+                pivot.add(object);
 
-			function animate() {
+            }, onProgress, onError);
 
-				requestAnimationFrame( animate );
-				render();
+        });
 
-			}
+        mtlLoader = new THREE.MTLLoader();
+        mtlLoader.setPath('3d/');
+        mtlLoader.load('oar4.mtl', function (materials) {
 
-			function render() {
+            materials.preload();
 
-				camera.position.x += ( mouseX - camera.position.x ) * .05;
-				//camera.position.y += 10 + ( - mouseY - camera.position.y ) * .05;
-                oar_pivot.rotation.y = mouseY / 100 * Math.PI;
-				camera.lookAt( scene.position );
+            var objLoader = new THREE.OBJLoader();
+            objLoader.setMaterials(materials);
+            objLoader.setPath('3d/');
+            objLoader.load('oar4.obj', function (object2) {
+                object2.position.z = -13.5;
+                object2.position.x = -22.7;
+                oar_pivot.add(object2);
 
-				renderer.render( scene, camera );
 
-			}
-	}
+            }, onProgress, onError);
+
+        });
+
+        oar_pivot.position.z = 13.5;
+        oar_pivot.position.x = 22.7;
+
+
+        //
+
+        renderer = new THREE.WebGLRenderer();
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        renderer.setClearColor(0xFFFFFF);
+        container.appendChild(renderer.domElement);
+
+        document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+        //
+
+        window.addEventListener('resize', onWindowResize, false);
+
+    }
+
+    function onWindowResize() {
+
+        windowHalfX = window.innerWidth / 2;
+        windowHalfY = window.innerHeight / 2;
+
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+    }
+
+    function onDocumentMouseMove(event) {
+
+        mouseX = (event.clientX - windowHalfX) / 8;
+        mouseY = (event.clientY - windowHalfY) / 8;
+
+    }
+
+    //
+
+    function animate() {
+
+        requestAnimationFrame(animate);
+        render();
+
+    }
+
+    function render() {
+
+        camera.position.x += (mouseX - camera.position.x) * .05;
+        //camera.position.y += 10 + ( - mouseY - camera.position.y ) * .05;
+        oar_pivot.rotation.y = mouseY / 100 * Math.PI;
+        camera.lookAt(scene.position);
+
+        renderer.render(scene, camera);
+
+    }
+}
 );
