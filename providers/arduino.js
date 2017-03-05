d603cc8b5e19abf63ed20114bb9304f067b9b78c
Jukka Ylitalo <juylitalo@gmail.com>
2017-03-05 19:44:31 +0200

    log rowing

--- providers/arduino.js
+++ providers/arduino.js
@@ -1,26 +1,43 @@
-
-var serial = require( "serialport" );
+var fs = require("fs");
+var serial = require("serialport");
 var SerialPort = serial.SerialPort;
 
 // Replace with the device name in your machine.
 var portName = "COM5"; // /dev/cu.usbmodem1421
 
-var sp = new SerialPort( portName, {
-	baudrate:9600,
-	parser  :serial.parsers.readline( "\n" )
-} );
+var sp = new SerialPort(portName, {
+	baudrate: 9600,
+	parser: serial.parsers.readline("\n")
+});
+
+var scriptStartTime = Date.now();
+var prevTime = scriptStartTime;
 
 module.exports = {
 
-	init:function ( socket ) {
+	init: function (socket, params) {
+		var log2file = params.logging;
+		if (params.oarsActive) {
 
-		/* When we get a new line from the arduino, send it to the browser via this socket */
-		sp.on( "data", function ( data ) {
-			console.log( data );
-			socket.emit( "message", data.toString() );
-		} );
+			/* When we get a new line from the arduino, send it to the browser via this socket */
+			sp.on("data", function (data) {
+				var nowtime = Date.now();
 
+				if (prevTime != nowtime) {
+					var datatoCollect = Date.now() + ":" + data;
+					console.log(datatoCollect);
+					if (log2file) {
+						fs.appendFile('logs/rowsessions/rowlog_' + scriptStartTime + ".txt", datatoCollect, function (err) {
+							//console.log(err);
+						});
+					}
+				}
+				prevTime = nowtime;
+				// console.log(data);
+				socket.emit("message", data.toString());
+			});
+
+		}
 	}
-
 };
 
--- row.js
+++ row.js
@@ -1,7 +1,25 @@
 
 var fs = require( "fs" );
 var url = require( "url" );
+var stdio = require('stdio');
 
+
+var $parameterData = { "oarsActive": 0, "logging": 0 };
+
+var ops = stdio.getopt({
+	'oarsActive': { key: 'o', description: 'oars active or not y/n' },
+	'logging': { key: 'l', description: 'Activate logging' },
+});
+
+console.dir(ops);
+if (ops.oarsActive != undefined) {
+	$parameterData.oarsActive = ops.oarsActive ;
+}
+if (ops.logging != undefined) {
+	$parameterData.logging = ops.logging ;
+}
+console.dir($parameterData);
+//process.exit(); 
 
 /* Create the server in the port 9000 */
 var http = require( "http" ).createServer(function ( req, res ) {
@@ -39,6 +57,6 @@ io.set('log level', 1);
 
 io.sockets.on( "connection", function ( socket ) {
 	// On a new Socket.io connection, load the data provider we want. For now, just Arduino.
-	var $provider = require( './providers/arduino.js' ).init( socket );
+	var $provider = require( './providers/arduino.js' ).init( socket, $parameterData );
 } );
 
