c4ab212d1eb0c234776f739ed1dbf9004e502d6d
Jukka Ylitalo <juylitalo@gmail.com>
2017-03-14 18:31:07 +0200

    iterating com ports

--- providers/arduino.js
+++ providers/arduino.js
@@ -1,14 +1,17 @@
 var fs = require("fs");
-var serial = require("serialport");
-var SerialPort = serial.SerialPort;
 
-// Replace with the device name in your machine.
-var portName = "COM5"; // /dev/cu.usbmodem1421
 
-var sp = new SerialPort(portName, {
-	baudrate: 9600,
-	parser: serial.parsers.readline("\n")
-});
+
+var SerialPort = require('serialport');
+var portNames = ["COM5", "COM6", "COM1", "COM2", "COM3", "COM4"];
+var portName = portNames[0]; // 
+// process.exit();
+console.log("configuring serial port");
+var sp;
+
+var portIterateCount = 0;
+
+
 
 var scriptStartTime = Date.now();
 var prevTime = scriptStartTime;
@@ -16,28 +19,62 @@ var prevTime = scriptStartTime;
 module.exports = {
 
 	init: function (socket, params) {
-		var log2file = params.logging;
-		if (params.oarsActive) {
+		console.log("arduino init");
 
-			/* When we get a new line from the arduino, send it to the browser via this socket */
-			sp.on("data", function (data) {
-				var nowtime = Date.now();
 
-				if (prevTime != nowtime) {
-					var datatoCollect = Date.now() + ":" + data;
-					console.log(datatoCollect);
-					if (log2file) {
-						fs.appendFile('logs/rowsessions/rowlog_' + scriptStartTime + ".txt", datatoCollect, function (err) {
-							//console.log(err);
+
+		function initSerial(portName) {
+			console.log("trying with " + portNames[portIterateCount]);
+			sp = new SerialPort(portName, {
+				baudrate: 9600,
+				parser: SerialPort.parsers.readline("\n"),
+				autoOpen: false
+			});
+			sp.on('error', function (err) {
+				console.log('Error1: ', err.message);
+
+			});
+
+			sp.open(function (err) {
+				if (err) {
+					// console.log('Error opening port: ', err.message);
+					portIterateCount++;
+
+					portName = portNames[portIterateCount]
+					initSerial(portName);
+				} else {
+
+					console.log(portName + ' port opened ');
+					var log2file = params.logging;
+					if (params.oarsActive) {
+
+						/* When we get a new line from the arduino, send it to the browser via this socket */
+						sp.on("data", function (data) {
+							var nowtime = Date.now();
+
+							if (prevTime != nowtime) {
+								var datatoCollect = Date.now() + ":" + data;
+								console.log(datatoCollect);
+								if (log2file) {
+									fs.appendFile('logs/rowsessions/rowlog_' + scriptStartTime + ".txt", datatoCollect, function (err) {
+										//console.log(err);
+									});
+								}
+							}
+							prevTime = nowtime;
+							// console.log(data);
+							socket.emit("message", data.toString());
 						});
+
 					}
 				}
-				prevTime = nowtime;
-				// console.log(data);
-				socket.emit("message", data.toString());
 			});
 
 		}
+
+		initSerial(portName);
+
+
 	}
 };
 
